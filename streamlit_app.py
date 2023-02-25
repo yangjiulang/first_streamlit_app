fa4b50713fe6b508c6082ccef54f04ce9ecb0fc1
yangjiulang <yangjiulang@hotmail.com>
2023-02-25 16:37:37 +1300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 
 streamlit.title('My Parents New Health Diner')
-streamlit.title('Breakfast Menu')
-streamlit.title('Omega 3 & Blueberry Oatmeal')
-streamlit.title('Kale,Spinach & Rocket Smoothie')
-streamlit.title('Hard-Boiled Free-Range Egg')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale,Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
