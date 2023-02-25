c58bc7346416b8ff35b56445edfd30d11bc9f91f
yangjiulang <yangjiulang@hotmail.com>
2023-02-25 16:36:54 +1300

    Update streamlit_app.py
    Added Menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('My Parents New Health Diner')
+streamlit.title('Breakfast Menu')
+streamlit.title('Omega 3 & Blueberry Oatmeal')
+streamlit.title('Kale,Spinach & Rocket Smoothie')
+streamlit.title('Hard-Boiled Free-Range Egg')
